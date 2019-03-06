112e52296ef255aab14bd88f265293eb5b0af908
Lee Briggs <lbriggs@apptio.com>
2019-03-06 14:17:04 -0800

    Set consulservers

--- modules/profiles/manifests/base.pp
+++ modules/profiles/manifests/base.pp
@@ -1,5 +1,5 @@
 class profiles::base(
-
+  Array[String] $consul_servers
 ){
 
   class { '::profiles::packages':
@@ -19,7 +19,7 @@ class profiles::base(
 
   unbound::forward { 'service.consul':
     # We use suffix from stdlib to add the consul DNS port: https://github.com/puppetlabs/puppetlabs-stdlib#suffix
-    address => suffix($consul_cluster_nodes, '@8600')
+    address => suffix($consul_servers, '@8600')
   }
 
   unbound::forward { '.':
