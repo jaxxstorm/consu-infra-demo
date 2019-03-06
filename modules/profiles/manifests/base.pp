61220ad8d34611ac83d103902a417a5a543ab40d
Lee Briggs <lbriggs@apptio.com>
2019-03-06 14:06:12 -0800

    Use unbound instead of coredns

--- modules/profiles/manifests/base.pp
+++ modules/profiles/manifests/base.pp
@@ -10,10 +10,20 @@ class profiles::base(
 
   include ::profiles::consul
 
-  include ::profiles::coredns
+  include ::unbound
 
-  class { 'resolv_conf':
-    nameservers => ['127.0.0.1', '173.245.58.51', '8.8.8.8'],
+  unbound::stub { $::domain :
+    address  => '173.245.58.51',
+    insecure => true,
+  }
+
+  unbound::forward { 'service.consul':
+    # We use suffix from stdlib to add the consul DNS port: https://github.com/puppetlabs/puppetlabs-stdlib#suffix
+    address => suffix($consul_cluster_nodes, '@8600')
+  }
+
+  unbound::forward { '.':
+    address => [ '8.8.8.8', '8.8.4.4' ]
   }
 
 
